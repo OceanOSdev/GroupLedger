bb2364897234387d6cb24103bfb09a7d2be97c07
Thomas Maloney <ThomasTNF@live.com>
2017-10-04 17:54:46 -0500

    Users are now unable to add negative-valued transactions

--- Group Ledger/Models/TransactionViewModel.cs
+++ Group Ledger/Models/TransactionViewModel.cs
@@ -9,6 +9,7 @@ namespace Group_Ledger.Models
         [Display(Name = "Last Name")]
         public string ToLastName { get; set; }
         [Display(Name = "Amount ($)")]
+        [Range(typeof(decimal),"0", "39614081257132168796771975168", ErrorMessage = "You cannot send a negative amount")]
         public decimal Amount { get; set; }
     }
 }
